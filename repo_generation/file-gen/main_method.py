d5f54d6c85888861e9a9d33e9e69276e032d752f
afluffybunny7 <andrewalfredparr@yahoo.com>
2025-03-17 14:53:22 -0400

    [Refactor] Made my files use yml instead of an input stream

--- repo_generation/file-gen/main_method.py
+++ repo_generation/file-gen/main_method.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from abc import ABC, abstractmethod
+import yaml
 import os
 import shutil
 from typing import List
@@ -33,7 +34,7 @@ class RepositoryLevelYmlFile(YmlFile):
         if any(not module or not module.strip() for module in modules):
             raise ValueError("Invalid module name")
             
-        self.type = "more" if not type.strip() else type
+        self.type = "more" if type==None else type
         self.password = password
         self.award = award
         self.image = "pwncollege/challenge-simple" if not image.strip() else image
@@ -57,7 +58,7 @@ class RepositoryLevelYmlFile(YmlFile):
         return result
 
 class Challenge:
-    def __init__(self, id: str, name: str, files: List[str] = None, parent = None):
+    def __init__(self, id: str, name: str, files: List[str], parent = None):
         self.id = id
         self.name = name
         self.files = files
@@ -86,36 +87,35 @@ class ModuleLevelYml(YmlFile):
 def build_repo_yml(input_source) -> RepositoryLevelYmlFile:
     #print("Hello! I'm here to help you build your command line CTF.\n")
     #print("What should its ID be? It must be unique (different from the other available CTFs).")
-    id = input_source.readline().strip()
+    id = input_source["id"]
     
     #print("What would you like the name of your CTF to be?")
-    name = input_source.readline().strip()
+    name = input_source["name"]
     
     #print("Would you like a description? If not, press enter")
-    description = input_source.readline().strip()
+    description = input_source["description"]
     
     #print("What will the type be? [course/topic/hidden] or just press enter.")
-    type = input_source.readline().strip()
-    while type and type not in ["course", "topic", "hidden"]:
-        #print("We couldn't determine your response. What will the type be? [course/topic/hidden] or just press enter.")
-        type = input_source.readline().strip()
+    type = input_source["type"]
+    
     
     #print("Would you like a password? If not, press enter.")
-    password = input_source.readline().strip()
+    password = input_source["password"]
     
     #print("What would you like the award to be? Please enter an emoji")
-    award = input_source.readline().strip()
+    award = input_source["award"]
     
     #print("What would you like the image path to be? Press enter for default.")
-    image = input_source.readline().strip()
+    image = input_source["image"]
+    if image == None:
+        image = ""
     
     #print("Please enter the names of your modules 1 at a time. Submit a blank name when done.")
     modules = []
-    while True:
-        module = input_source.readline().strip()
-        if not module:
-            break
-        modules.append(module)
+    if  input_source["modules"] == None:
+        raise ValueError("Modules are required")
+    for arr in input_source["modules"]:
+        modules.append(arr[0])
     
     return RepositoryLevelYmlFile(id, name, description, type, password, award, image, modules)
 
@@ -123,11 +123,10 @@ def get_code(input_source, modules: List[ModuleLevelYml]):
     for m in modules:
         for c in m.challenges:
             #print(f'For your module named "{m.name}", what files would you like to use? Input a blank when done.')
-            while True:
-                file_path = input_source.readline().strip()
-                if not file_path:
-                    break
-                    
+            file_paths = c.files
+            if not file_paths:
+                break
+            for file_path in file_paths:
                 source = os.path.abspath(file_path)
                 dest_dir = os.path.join(baseRepoDir, m.name.lower(), c.id)
                 dest = os.path.join(dest_dir, os.path.basename(source))
@@ -140,22 +139,29 @@ def get_code(input_source, modules: List[ModuleLevelYml]):
 def get_module_level(input_source, rlyf: RepositoryLevelYmlFile) -> List[ModuleLevelYml]:
     module_level_ymls = []
     
-    for module in rlyf.modules:
+    for module in input_source["modules"]:
         #print(f'For your module named "{module}", what would you like its ID to be?')
-        id = input_source.readline().strip()
+        id = module[1]
         
         #print("Would you like a description? If not, press enter")
-        description = input_source.readline().strip()
+        description = module[2]
         
         #print("Please enter the names of your challenges 1 at a time. Submit a blank name when done.")
         challenges = []
-        while True:
-            challenge = input_source.readline().strip()
-            if not challenge:
-                break
-            challenges.append(Challenge(challenge.lower(), challenge))
+        if(len(module[3])) == 0:
+            raise ValueError("Must have challenges in your module")
+        for chal in module[3]:
+            if(chal[0] == None or chal[1] == None):
+                raise ValueError("Must have a name for your challenges")
+            for file in chal[1]:
+                try:
+                    open(file, "r")
+                except:
+                    raise ValueError("Filename des not exist")
+            challenges.append(Challenge(chal[0].lower(), chal[0], chal[1]))
+
         
-        module_level_ymls.append(ModuleLevelYml(id, module, description, challenges))
+        module_level_ymls.append(ModuleLevelYml(id, module[0], description, challenges))
     
     return module_level_ymls
 
@@ -187,23 +193,27 @@ def write_to_files(rlyf: RepositoryLevelYmlFile, module_level_ymls: List[ModuleL
 def main(filename):
     import sys
     # Handle input source (file or stdin)
+    with open(sys.argv[1], "r") as file:
+        input_source = yaml.safe_load(file)
     if len(sys.argv) > 1:
+        print("Here")
         try:
-            input_source = open(sys.argv[1], 'r')
+            with open(sys.argv[1], "r") as file:
+                input_source = yaml.safe_load(file)
+                print("Here")
         except Exception as e:
             if filename:
                 input_source = open(filename, 'r')
             else:
                 input_source = sys.stdin
+    
+    
     print(input_source)
-    try:
-        rlyf = build_repo_yml(input_source)
-        module_level_ymls = get_module_level(input_source, rlyf)
-        write_to_files(rlyf, module_level_ymls)
-        get_code(input_source, module_level_ymls)
-    finally:
-        if input_source != sys.stdin:
-            input_source.close()
+    rlyf = build_repo_yml(input_source)
+    module_level_ymls = get_module_level(input_source, rlyf)
+    write_to_files(rlyf, module_level_ymls)
+    get_code(input_source, module_level_ymls)
+            
 
 if __name__ == "__main__":
     
